6db591d60ccdc4a7e3a526a5ca891d7dc3ca6680
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-07-04 12:18:01 -0600

    Se agregaron las pruebas unitarias de la clase Clac

--- test/CalcTest.java
+++ test/CalcTest.java
@@ -1,5 +1,30 @@
 package test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import src.Calc;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalcTest {
-    
+    @Test
+    public void testSuma() {
+        assertEquals(5, Calc.suma(2, 3));
+        assertEquals(-1, Calc.suma(-2, 1));
+        assertEquals(0, Calc.suma(0, 0));
+    }
+
+    @Test
+    public void testResta() {
+        assertEquals(1, Calc.resta(3, 2));
+        assertEquals(-3, Calc.resta(-2, 1));
+        assertEquals(0, Calc.resta(0, 0));
+    }
+
+    @Test
+    public void testMultiplicacion() {
+        assertEquals(6, Calc.multiplicacion(2, 3));
+        assertEquals(-2, Calc.multiplicacion(-2, 1));
+        assertEquals(0, Calc.multiplicacion(0, 3));
+    }
 }
